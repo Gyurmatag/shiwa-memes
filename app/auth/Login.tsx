bc01032910e9853074e9fe5899ba6192dba0a3d1
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-03-30 21:56:50 +0200

    adding purple background to Login too

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -7,7 +7,7 @@ export default function Login() {
     <li className="list-none">
       <button
         onClick={() => signIn()}
-        className="rounded-xl bg-gray-700 py-2 px-6 text-sm text-white disabled:opacity-25"
+        className="rounded-xl bg-shiwa-purple py-2 px-6 text-sm text-white disabled:opacity-25"
       >
         Sign In
       </button>
