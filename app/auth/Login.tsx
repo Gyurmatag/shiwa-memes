4f30f8651f9c58e4ee06b1af9b712935199ea656
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-04-02 15:02:52 +0200

    adding hover state styles too

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -7,7 +7,7 @@ export default function Login() {
     <li className="list-none">
       <button
         onClick={() => signIn()}
-        className="rounded-xl bg-shiwa-purple py-2 px-6 text-sm text-white hover:opacity-75 disabled:opacity-25"
+        className="rounded-xl bg-shiwa-purple py-2 px-6 text-sm text-white hover:opacity-90 disabled:opacity-25"
       >
         Sign In
       </button>
