592ed39aaaabb52cf92bde2e2c49f900c6eb74a5
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-04-02 14:44:07 +0200

    add hover state style for interactive elements

--- app/new-meme/AddMeme.tsx
+++ app/new-meme/AddMeme.tsx
@@ -89,7 +89,7 @@ export default function AddMeme() {
           />
           <label
             htmlFor="image-upload"
-            className="flex cursor-pointer items-center justify-center rounded-md bg-gray-200 p-4 text-lg"
+            className="flex cursor-pointer items-center justify-center rounded-md bg-gray-200 p-4 text-lg hover:bg-gray-300"
           >
             {imgFile && imgPreview ? (
               <Image
@@ -111,7 +111,7 @@ export default function AddMeme() {
       <div className="flex justify-center">
         <button
           disabled={isDisabled}
-          className="rounded-xl bg-teal-600 py-2 px-6 text-sm text-white disabled:opacity-25"
+          className="rounded-xl bg-teal-600 py-2 px-6 text-sm text-white hover:bg-teal-700 disabled:opacity-25"
           type="submit"
         >
           Create meme
